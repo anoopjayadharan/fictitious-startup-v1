cf3a36fb63af69d9dd501d9305faf757c0ca20b1
Anoopdharan1 <anoopdharan1@gmail.com>
2024-11-08 20:31:42 +0300

    removed random plugin

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,16 +1,14 @@
+# Terraform block
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "5.73.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.6.3"
-    }
   }
 }
 
+# Backend Terraform Cloud
 terraform {
   cloud {
 
